924a9554022f476376c310428ec6d1e01c008cf6
v_qinzp <v_qinzp@fosun.com>
2018-07-25 15:32:01 +0800

    '项目架构基础搭建'

---
+++ src/components/hello/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <one></one>
+      <tow></tow>
+  </div>
+</template>
+
+<script>
+import one from './view/one'
+import tow from './view/tow'
+export default {
+    components: {
+        one,
+        tow
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
---
+++ src/components/hello/view/tow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tow">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your tow Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+    @import '../style/tow.less';
+</style>
